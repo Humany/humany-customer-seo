0d8cbd5e0ab7d5a9a7e93b86915925bf2b14d2a7
JWMB <jonas.beckeman@gmail.com>
2019-09-23 13:44:34 +0200

    Use WidgetConfiguration from seo-service/customer

--- humany-customer-seo-netcore/WidgetConfiguration.cs
+++ humany-customer-seo-netcore/WidgetConfiguration.cs
@@ -7,9 +7,9 @@ namespace humany_customer_seo_netcore
 {
 	public class WidgetConfiguration
 	{
-		public string HumanyTenantDomain { get => $"{Tenant}.humany.cc"; }
+		public string GetHumanyTenantDomain(string topDomain) => $"{Tenant}.humany.{topDomain}";
 		public string Tenant { get; set; } = string.Empty;
-		public string Interface { get; set; } = string.Empty;
+		public string WidgetUriName { get; set; } = string.Empty;
 		public string Implementation { get; set; } = string.Empty;
 		public string BasePath { get; set; } = string.Empty;
 
@@ -17,17 +17,17 @@ namespace humany_customer_seo_netcore
 		{
 		}
 
-		public WidgetConfiguration(string tenant, string interface_, string implementation, string basePath = "/")
+		public WidgetConfiguration(string tenant, string widgetUriName, string implementation, string basePath = "/")
 		{
 			Tenant = tenant;
-			Interface = interface_;
+			WidgetUriName = widgetUriName;
 			Implementation = implementation;
 			BasePath = basePath;
 		}
 
 		public bool IsValid()
 		{
-			return !string.IsNullOrWhiteSpace(Tenant) && !string.IsNullOrWhiteSpace(Interface) && !string.IsNullOrWhiteSpace(Implementation);
+			return !string.IsNullOrWhiteSpace(Tenant) && !string.IsNullOrWhiteSpace(WidgetUriName) && !string.IsNullOrWhiteSpace(Implementation);
 		}
 		public static string GetPathInWidget(string pageName, string requestPath)
 		{
